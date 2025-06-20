1dfecdc12932f77a215e50ab608b8cbb251f80b4
Murat Goksenin Guzel <murat.guzel@umbrage.com>
2025-06-20 13:25:08 -0500

    ICommand and IQuery Interfaces generated to segregate request/response form mediatr Irequest class.

---
+++ src/Framework/Framework/CQRS/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+
+namespace Framework.CQRS
+{
+    //This interface retunrs nothing MGG 6/20/2025
+    public interface ICommand<out TResponse> :IRequest<TResponse>
+    {
+    }
+
+
+    //This interface retunrs responce MGG 6/20/2025
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+
+
+}
