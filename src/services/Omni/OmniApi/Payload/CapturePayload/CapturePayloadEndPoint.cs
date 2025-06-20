d028898aae56c019cb07e7c632af332eaefae364
Murat Goksenin Guzel <murat.guzel@umbrage.com>
2025-06-20 15:01:42 -0500

    Carter Router implemented on framework and inside handler

--- src/services/Omni/OmniApi/Payload/CapturePayload/CapturePayloadEndPoint.cs
+++ src/services/Omni/OmniApi/Payload/CapturePayload/CapturePayloadEndPoint.cs
@@ -1,9 +1,17 @@
-﻿namespace OmniApi.Payload.CapturePayload
+﻿using Carter;
+
+namespace OmniApi.Payload.CapturePayload
 {
     //Vertical slice architecture presentation API layer-MGG 6/20
-    public class CapturePayloadEndPoint
+    
+    public record CaptureDevicePayload_Request(DateTime Timestamp, string DataType, double Value, List<object> Properties);
+
+
+    public class CapturePayloadEndPoint : ICarterModule
     {
-
-
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
