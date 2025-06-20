3ff9f078db5a8ff81b87c0201f61d14a0c55cb2d
Murat Goksenin Guzel <murat.guzel@umbrage.com>
2025-06-20 13:01:52 -0500

    CapturePayloadCommandHandler Implemented

--- src/services/Omni/OmniApi/Payload/CapturePayload/CapturePayloadHandler.cs
+++ src/services/Omni/OmniApi/Payload/CapturePayload/CapturePayloadHandler.cs
@@ -1,12 +1,19 @@
-﻿namespace OmniApi.Payload.CapturePayload
-{
+﻿using MediatR;
 
-    public record CapturePayload_Command(DateTime Timestamp, string DataType, double Value, List<object> Properties);
+namespace OmniApi.Payload.CapturePayload
+{
+    //Inherits from Mediatr IRequest Class
+    public record CapturePayload_Command(DateTime Timestamp, string DataType, double Value, List<object> Properties) 
+        :IRequest<CapturePayload_Result>;
     public record CapturePayload_Result(Guid Id);
 
     //Link Payload MGG 6/20    
-    public class CapturePayloadHandler
+    internal class CapturePayloadCommandHandler : IRequestHandler<CapturePayload_Command, CapturePayload_Result>
     {
-
+        public Task<CapturePayload_Result> Handle(CapturePayload_Command request, CancellationToken cancellationToken)
+        {
+            //Business logic MGG 6/20
+            throw new NotImplementedException();
+        }
     }
 }
