78511cf2c35ae0d1e52d817343874c7ea9ce2da3
Murat Goksenin Guzel <murat.guzel@umbrage.com>
2025-06-20 12:42:26 -0500

    Command and Result Objects Created

--- src/services/Omni/OmniApi/Payload/CapturePayload/CapturePayloadHandler.cs
+++ src/services/Omni/OmniApi/Payload/CapturePayload/CapturePayloadHandler.cs
@@ -1,6 +1,12 @@
 ﻿namespace OmniApi.Payload.CapturePayload
 {
+
+    public record CapturePayload_Command(DateTime Timestamp, string DataType, double Value, List<object> Properties);
+    public record CapturePayload_Result(Guid Id);
+
+    //Link Payload MGG 6/20    
     public class CapturePayloadHandler
     {
+
     }
 }
