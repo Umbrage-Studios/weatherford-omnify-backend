df2133543d08cc42e2cd5f3a31d1b177f913bd6d
Murat Goksenin Guzel <muratgguzel77@yahoo.com>
2025-06-22 07:26:55 -0500

    GetPayloads EndPoint Implementation

---
+++ src/services/Omni/OmniApi/Payload/GetPayload/GetPayloadHandler.cs
@@ -0,0 +1,18 @@
+﻿using Framework.CQRS;
+using Marten.Linq.QueryHandlers;
+using OmniApi.Models;
+
+namespace OmniApi.Payload.GetPayload
+{
+    public record GetPayloadsQuery():IQuery<GetPayloadsResult>;
+
+    public record GetPayloadsResult(IEnumerable<DevicePayload> DevicePayloads);
+
+    internal class GetPayloadsQueryHandler : IQueryHandler<GetPayloadsQuery, GetPayloadsResult>
+    {
+        public Task<GetPayloadsResult> Handle(GetPayloadsQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
