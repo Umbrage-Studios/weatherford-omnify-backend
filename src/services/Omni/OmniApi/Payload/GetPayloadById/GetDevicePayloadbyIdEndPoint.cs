8510a55ee979abae482867115793932b9cada49d
Murat Goksenin Guzel <muratgguzel77@yahoo.com>
2025-06-22 20:23:13 -0500

    GetPayloadWithId Implemented

---
+++ src/services/Omni/OmniApi/Payload/GetPayloadById/GetDevicePayloadbyIdEndPoint.cs
@@ -0,0 +1,24 @@
+﻿using OmniApi.Models;
+
+namespace OmniApi.Payload.GetPayloadById
+{
+
+    public record GetDevicePayloadbyIdResponse(DevicePayload DevicePayload);
+    public class GetDevicePayloadbyIdEndPoint :ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/devicepayloads/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetDevicePayloadbyIdQuery(id));
+                var response = result.Adapt<GetDevicePayloadbyIdResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetDevicePayloadById")
+            .Produces<GetDevicePayloadbyIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Device Payload by ID")
+            .WithDescription("Retrieves a specific Weatherford IoT device payload by its ID from the database.");
+        }
+    }
+}
---
+++ src/services/Omni/OmniApi/Payload/GetPayloadById/GetDevicePayloadbyIdHandler.cs
@@ -0,0 +1,30 @@
+﻿using Framework.CQRS;
+using OmniApi.Exceptions;
+using OmniApi.Models;
+
+namespace OmniApi.Payload.GetPayloadById
+{
+    public record GetDevicePayloadbyIdQuery(Guid Id) :IQuery<GetDevicePayloadbyIdResult>;
+
+    public record GetDevicePayloadbyIdResult(DevicePayload DevicePayload);
+    internal class GetDevicePayloadbyIdQueryHandler(IDocumentSession session, ILogger<GetDevicePayloadbyIdQueryHandler> logger) 
+        : IQueryHandler<GetDevicePayloadbyIdQuery, GetDevicePayloadbyIdResult>
+        
+    {
+        public async Task<GetDevicePayloadbyIdResult> Handle(GetDevicePayloadbyIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Weatherford Device logs by GetPayloadsQueryHandler {@Query}", query);
+
+            var devicePayload = await session.LoadAsync<DevicePayload>(query.Id, cancellationToken);
+
+            if (devicePayload == null)
+            {
+                logger.LogWarning("Device payload with ID {Id} not found.", query.Id);
+                throw new DeviceNotFoundException();
+            }
+
+            return new GetDevicePayloadbyIdResult(devicePayload);
+
+        }
+    }
+}
